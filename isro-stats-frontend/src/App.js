6548280ea96f6012d4d2c215d343c58bd56cb805
disha-baghel <bagheldisha708@gmail.com>
2024-01-10 22:47:48 +0530

    corrected the frontend code

--- isro-stats-frontend/src/App.js
+++ isro-stats-frontend/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    async function fetchLaunches() {
+      try {
+        // Make a GET request to your Express.js backend API endpoint
+        const response = await axios.get('http://localhost:3001');
+        console.log('API Response: ',response.data);
+        setLaunches(response.data);
+      } catch (error) {
+        console.error('Error fetching launches:', error.message);
+      }
+    }
+
+    fetchLaunches();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ISRO Launches</h1>
+      <ul>
+        {launches.map((launch, index) => (
+          <li key={index}>{launch.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
